ae00f82bfa6cf9e46bcf508a6dec5530087a1465
xzhuah <xzhuah@connect.ust.hk>
2015-10-10 01:11:14 +0800

    commit3

--- Style/function.js
+++ Style/function.js
@@ -84,4 +84,65 @@ function appendOther(){
 	 anothetTitle.appendChild(say2);
 	 document.getElementById("append").appendChild(anothetTitle);
 }
-
+function prepareSlideshow(){
+	var preview=document.getElementById("preview");
+	preview.style.position="absolute";
+	preview.style.left="0px";
+	preview.style.top="0px";	
+	
+	var list=document.getElementById("linklist");
+	var links=list.getElementsByTagName("li");
+	links[0].onmouseover=function(){
+		moveElement("preview",0,0,10);
+	}
+	links[1].onmouseover=function(){
+		moveElement("preview",-235,0,10);
+	}
+	links[2].onmouseover=function(){
+		moveElement("preview",-470,0,10);
+	}
+	links[3].onmouseover=function(){
+		moveElement("preview",-710,0,10);
+	}
+}
+addLoadEvent(prepareSlideshow);
+function moveElement(elementID,final_x,final_y,interval){
+	var elem=document.getElementById(elementID);
+	if(elem.movement){
+		clearTimeout(elem.movement);
+	}
+	var xpos=parseInt(elem.style.left);
+	var ypos=parseInt(elem.style.top);
+	if(xpos==final_x&&ypos==final_y) return true;
+	if(xpos<final_x){
+		dist=Math.ceil((final_x-xpos)/10);
+		xpos=xpos+dist;
+	}
+	if(xpos>final_x){
+		dist=Math.ceil((xpos-final_x)/10);
+		xpos=xpos-dist;
+	}
+	if(ypos<final_y){
+		dist=Math.ceil((final_y-ypos)/10);
+		ypos=ypos+dist;
+	}
+	if(ypos>final_y){
+		dist=Math.ceil((ypos-final_y)/10);
+		ypos=ypos-dist;
+	}
+	elem.style.left=xpos+"px";
+	elem.style.top=ypos+"px";
+	var repeat="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+	elem.movement=setTimeout(repeat,interval);
+}
+function addLoadEvent(func) { 
+	var oldonload = window.onload; 
+		if (typeof window.onload != 'function') { 
+			window.onload = func; 
+			} else { 
+			window.onload = function() { 
+			oldonload(); 
+			func(); 
+		} 
+	} 
+}  
